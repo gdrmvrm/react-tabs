9620749343d1b36ab3d35d593205ab1cad73ddf7
gdrmvrm <gdrmvrm@gmail.com>
2025-10-08 10:08:26 +0100

    Fetch jobs and add loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from 'react';
+
 const url = 'https://www.course-api.com/react-tabs-project';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="job-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
   return <h2>Tabs Starter</h2>;
 };
 export default App;
